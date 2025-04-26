cb899f72dff682c71b5e3239e7740b5a4c9bb3da
LeandroArantesDev <leandro.arantes@edu.uniube.br>
2025-04-25 22:31:30 -0300

    Atualização do cabeçalho e logo: alterado fundo do cabeçalho para branco, ajustadas cores e transições, e substituída a imagem do logo.

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,13 +1,9 @@
 const header = document.querySelector("header");
-const fonte = document.querySelector("header a");
-const fontei = document.querySelector("header i");
 
 window.addEventListener("scroll", function () {
     if (this.scrollY > 100) {
         header.classList.add("scrolled");
-        fonte.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
-        fonte.classList.remove("scrolled");
     }
 })

000fcbbcb423f3f27ff640bd4ac4f14c1a9f87a8
LeandroArantesDev <leandro.arantes@edu.uniube.br>
2025-04-25 22:12:35 -0300

    teste

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,15 +1,13 @@
 const header = document.querySelector("header");
-const logo = document.getElementById("logo");
 const fonte = document.querySelector("header a");
+const fontei = document.querySelector("header i");
 
 window.addEventListener("scroll", function () {
     if (this.scrollY > 100) {
         header.classList.add("scrolled");
-        logo.src = "assets/images/logo_branco.png";
         fonte.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
-        logo.src = "assets/images/logo_preto.png";
         fonte.classList.remove("scrolled");
     }
 })

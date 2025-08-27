f2065acd84ebe10658d52aab5aba48a29025c24b
LeandroArantesDev <leandrohenriquerantesoficial@gmail.com>
2025-08-27 00:20:51 -0300

    Algumas atualizações

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,20 @@ btnMaisProjeto.addEventListener("click", () => {
         btnMaisProjeto.innerText = "Ver mais projetos";
     }, 3000);
 })
+
+// Botão do menu de celular
+
+const btn_mobile = document.getElementById("btn-mobile");
+const btn_mobile_icon = document.getElementById("btn-mobile-icon");
+const menu_mobile = document.getElementById("links");
+
+btn_mobile.addEventListener("click", () => {
+    menu_mobile.classList.toggle("ativo");
+    if (btn_mobile_icon.classList.contains("bi-list")) {
+        btn_mobile_icon.classList.remove("bi-list");
+        btn_mobile_icon.classList.add("bi-x-lg");
+    } else {
+        btn_mobile_icon.classList.remove("bi-x-lg");
+        btn_mobile_icon.classList.add("bi-list");
+    }
+})

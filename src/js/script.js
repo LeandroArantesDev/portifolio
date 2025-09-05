713a7558ce5ef2ab765f990d289bc1b20f108f91
LeandroArantesDev <leandro.arantes@edu.uniube.br>
2025-09-05 00:31:57 -0300

    Atualização: modo mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,12 @@
-const btnMaisProjeto = document.getElementById("maisprojetos");
-
-btnMaisProjeto.addEventListener("click", () => {
-    btnMaisProjeto.innerText = "Em andamento";
-
-    setInterval(() => {
-        btnMaisProjeto.innerText = "Ver mais projetos";
-    }, 3000);
-})
-
+// Fecha o menu_mobile ao clicar em qualquer link do menu
+const links_menu = document.querySelectorAll('#links a');
+links_menu.forEach(link => {
+    link.addEventListener('click', () => {
+        menu_mobile.classList.remove('ativo');
+        btn_mobile_icon.classList.remove('bi-x-lg');
+        btn_mobile_icon.classList.add('bi-list');
+    });
+});
 // Botão do menu de celular
 
 const btn_mobile = document.getElementById("btn-mobile");
@@ -15,7 +14,6 @@ const btn_mobile_icon = document.getElementById("btn-mobile-icon");
 const menu_mobile = document.getElementById("links");
 
 btn_mobile.addEventListener("click", () => {
-    menu_mobile.classList.toggle("ativo");
     if (btn_mobile_icon.classList.contains("bi-list")) {
         btn_mobile_icon.classList.remove("bi-list");
         btn_mobile_icon.classList.add("bi-x-lg");
@@ -23,4 +21,5 @@ btn_mobile.addEventListener("click", () => {
         btn_mobile_icon.classList.remove("bi-x-lg");
         btn_mobile_icon.classList.add("bi-list");
     }
+    menu_mobile.classList.toggle("ativo");
 })
